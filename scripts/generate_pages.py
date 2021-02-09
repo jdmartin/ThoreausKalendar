4bf83662b14e484261978aecc6d87ed32ee6d6a7
jdmartin <jonsaccount+githubjd@icloud.com>
2021-02-09 13:42:52 -0800

    Added June 2

--- scripts/generate_pages.py
+++ scripts/generate_pages.py
@@ -245,8 +245,58 @@ def june():
         output.close()
         tail.close()
 
+def junetwo():
+    ##Process june2.xml
+
+    #Get the HTML for the top part of the file
+    with open("page_heads/june2", "r") as head:
+        top = head.read()
+        with open("june2.html", "w") as output:
+            output.write(top)
+        output.close()
+        head.close()
+
+    #Process the XML and output HTML elements for the body.
+    with open("data/june2.xml", "r") as file:
+        #Read the file
+        contents = file.read()
+        
+        #Make some soup
+        soup = BeautifulSoup(contents, 'lxml')
+
+        #Geat all the TEI rows
+        rows = soup.find_all("row")
+
+        with open("june2.html", "a") as output:
+            #Create a container for the processed and prepared XML
+            output.write('<table id="kalendar" border="1" cellpadding="1" cellspacing="0">')
+            for row in rows:
+                output.write("<tr>")
+
+                children = row.find_all("cell")
+
+                for elem in list(children):
+                    output.write("<td>")
+                    output.write(str(elem))
+                    output.write("</td>")
+
+                output.write("</tr>")
+
+            output.write("</table>")
+        output.close()
+        file.close()
+
+    #Get the HTML for the bottom of the file.
+    with open("page_tails/june2", "r") as tail:
+        bottom = tail.read()
+        with open("june2.html", "a") as output:
+            output.write(bottom)
+        output.close()
+        tail.close()
+
 newapril()
 otherapril()
 mayone()
 maytwo()
 june()
+junetwo()
