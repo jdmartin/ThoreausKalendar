9651ec7d57da6dee75bf5d83cb854539a388cb3e
jdmartin <jonsaccount+githubjd@icloud.com>
2021-10-21 12:38:24 -0700

    Removed cruft

--- resources/js/sticky.js
+++
@@ -1,9 +0,0 @@
-        $(window).bind("load", function() {
-            var $window = $(window),
-               $stickyEl = $('#search-menu'),
-               elTop = $stickyEl.offset().top;
-
-            $window.scroll(function() {
-                $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
-            });
-});
