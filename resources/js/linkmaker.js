752091374e369cfa82c1791969768756a98bbd56
jdmartin <jonsaccount+githubjd@icloud.com>
2021-10-24 14:41:03 -0700

    Apply full date as title attr on links

--- resources/js/linkmaker.js
+++ resources/js/linkmaker.js
@@ -8,11 +8,12 @@ $(document).ready(function(){
     $links.each( function (index, el) { //For each link element, keep track of which it is and reference it as the anonymous 'el' element
        $( el ).hasAttr('target'); //Does 'el' have a target attribute?
             var theStuff = $(this).parent('date').text(); //That is, grab the date from its parent date element
+            var theWhen = $(this).parent('date').attr('when');
             var theLink = $(this).attr('target'); //Grab the current link's target
             var str1 = '<a href="'; //The start of our link code.
-            var str2 = '" target="_blank" style="color: #8A4E5D;">'; //The end of our link element.  We put theLink between 1 and 2.
+            var str2 = '" target="_blank" style="color: #8A4E5D;" title="'; //The end of our link element.  We put theLink between 1 and 2.
             var str3 = '</a></date>'; //Close that link element.  We put the date just before this.
-            var link = str1.concat(theLink,str2,theStuff,str3); //Concatenate all the things!
+            var link = str1.concat(theLink,str2,theWhen,'">',theStuff,str3); //Concatenate all the things!
 
             $(this).parent('date').html(link); //Replace the parent 'date' element's content with the monster we just made.
        });
