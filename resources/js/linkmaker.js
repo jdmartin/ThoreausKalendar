c61ff83c55f36fc413c1c81377b89a6c0d2c377e
jdmartin <jonsaccount+githubjd@icloud.com>
2022-05-19 04:00:14 -0700

    Converted linkmaker.js

--- resources/js/linkmaker.js
+++ resources/js/linkmaker.js
@@ -1,20 +1,18 @@
-//jQuery doesn't have a hasAttribute function like JavaScript does.  So, let's make one:
-$.fn.hasAttr = function(name) {
-    return this.attr(name) !== undefined; //As long as it's not undefined, we go assume it's real.  Should probably check for false, too... next iteration!
-};
-
-$(document).ready(function(){
-    var $links = $('link');
-    $links.each( function (index, el) { //For each link element, keep track of which it is and reference it as the anonymous 'el' element
-       $( el ).hasAttr('target'); //Does 'el' have a target attribute?
-            var theStuff = $(this).parent('date').text(); //That is, grab the date from its parent date element
-            var theWhen = $(this).parent('date').attr('when'); //Get the full date from the parent's 'when' attribute
-            var theLink = $(this).attr('target'); //Grab the current link's target
+document.addEventListener("DOMContentLoaded", function(event) {
+    var links = document.getElementsByTagName("link");
+    for (var i = 0; i < links.length; i++) { 
+        
+        //Does current element have a target attribute?
+        if (links[i].hasAttribute('target')){
+            var theStuff = links[i].parentNode.innerText;
+            var theWhen = links[i].parentNode.getAttribute('when'); //Get the full date from the parent's 'when' attribute
+            var theLink = links[i].getAttribute('target'); //Grab the current link's target
             var str1 = '<a href="'; //The start of our link code.
             var str2 = '" target="_blank" style="color: #8A4E5D;" title="'; //The end of our link element.  We put theLink between 1 and 2.
             var str3 = '</a></date>'; //Close that link element.  We put the date just before this.
             var link = str1.concat(theLink,str2,theWhen,'">',theStuff,str3); //Concatenate all the things!
 
-            $(this).parent('date').html(link); //Replace the parent 'date' element's content with the monster we just made.
-       });
+            links[i].parentNode.innerHTML = link; //Replace the parent 'date' element's content with the monster we just made.
+        }; 
+       };
 });
